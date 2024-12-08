ba8ff1c279e35639691f9988a843223b88da2e19
a.guliganov <mizantronix@gmail.com>
2024-12-08 16:17:47 +0700

    calc

---
+++ calc-cli/cli/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"mathutils"
+)
+
+func main() {
+	var fg = mathutils.plus(1,3)
+    fmt.Println("Hello, World!", fg)
+}
---
+++ calc-cli/math-lib/go.mod
@@ -0,0 +1,3 @@
+module math-lib
+
+go 1.23.3
